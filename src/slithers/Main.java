62a12ce33cd7d08cbb76d11c1ff3cddaa3042ed6
dcancot111 <lolismr21@gmail.com>
2024-02-23 13:39:08 +0100

    subida final

--- src/slithers/Main.java
+++ src/slithers/Main.java
@@ -3,8 +3,22 @@ package slithers;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Slither s1 = new Slither("adri");
+		Slither s2 = new Slither("loli");
+		
+		s1.comer(Alimentos.VERDE);
+		s1.comer(Alimentos.NARANJA);
+		s1.comer(s2);
+		s1.mostrarInfo();
+		
+		s2.comer(s1);
+		s2.mostrarInfo();
+		
+		Slither s3 = new Slither("ansu");
+		s3.comer(Alimentos.VERDE);
+		s3.comer(Alimentos.ROJO);
+		s3.mostrarInfo();
+		
+		}
 
 }
